209ea14ae230ecd7ee3b351959f51e805a6bcf62
shyamala <shyamala_theni@yahoo.com>
2018-10-24 14:41:40 -0500

    Initialized project

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask  import Flask, request, redirect, render_template
+from flask  import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Build-a-blog:buildablog@localhost:8889/Build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True 
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
@@ -15,10 +16,65 @@ class Task(db.Model):
     def __init__(self, title, entry):
         self.title= title
         self.entry= entry
-
-#tasks_title = []
-#tasks_entry = []
 tasks = []
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            # TODO - explain why login failed
+            return '<h1>Error!</h1>'
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            # TODO - "remember" the user
+            return redirect('/')
+        else:
+            # TODO - user better response messaging
+            return "<h1>Duplicate user</h1>"
+    return render_template('signup.html')
 
 @app.route('/validate', methods=['POST','GET'])
 def validate_inputs():
@@ -26,10 +82,7 @@ def validate_inputs():
         blog_title = request.form['blog_title']
         blog_title_error = ''
         if (blog_title==''):
-            blog_title_error = ' Enter Blog title '
-        #else:
-        #    tasks_title.append(blog_title)
-           
+            blog_title_error = ' Enter Blog title '   
         blog_entry = request.form['blog_entry']
         blog_entry_error = ''
         if (blog_entry==''):
